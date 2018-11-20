a26afe7f026d30ee6a178eac1c051ece57e7b38f
beersonthewall <beersmatthew2314@gmail.com>
2018-11-19 20:37:36 -0600

    Add support for finding multiboot tags in the multiboot information structure.

--- src/kernel.c
+++ src/kernel.c
@@ -1,11 +1,15 @@
+#include <stddef.h>
 #include "terminal.h"
+#include "multiboot.h"
 
-void kstart(long multiboot) {
+void kstart(uint32_t* multiboot) {
     /* Initialize terminal interface */
     terminal_initialize();
-
     terminal_writestring("Hello, kernel World!\n"
-                         "MORE TO TEST NEWLINE");
-  for(;;){
-  }
+                         "MORE TO TEST NEWLINE\n");
+
+    memory_map* memInfo = (memory_map*) get_tag(MEM_MAP, multiboot);
+
+    terminal_writestring("MemoryMap found");
+    for(;;){}
 }
