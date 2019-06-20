3ea16a5858553639e67223da9112e14152e68cca
beersonthewall <beersmatthew2314@gmail.com>
2019-06-20 06:54:18 -0500

    Mark kernel and pysical memory bitmap as used pages.

--- src/memory.h
+++ src/memory.h
@@ -8,13 +8,9 @@
 #define ONE_KB 1024
 #define FOUR_KB 4096
 
-typedef struct mmap mmap;
-struct mmap {
-  void* begin_addr;
-  size_t size;
-  bool available;
-  mmap* next;
-};
+void pfree(void* paddr);
+
+void* palloc(size_t size);
 
 void mm_init(uint32_t* multiboot);
 
